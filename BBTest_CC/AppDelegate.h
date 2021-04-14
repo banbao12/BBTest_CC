30740eb4be4bfd3785ea322f501834c0f933f37e
Banbao12 <Mibao12@icloud.com>
2021-04-14 09:19:21 +0800

    tests

---
+++ BBTest_CC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  BBTest_CC
+//
+//  Created by 半饱 on 2021/4/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BBTest_CC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BBTest_CC
+//
+//  Created by 半饱 on 2021/4/14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
